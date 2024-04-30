44f2683d5f20a64c01a2c4d37d2732303dcddb1c
yudistiraramadan <yudistiraramadankalimasada@gmail.com>
2024-04-30 14:51:24 +0700

    make kuitansi seeder

---
+++ database/seeders/KuitansiSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kuitansi;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class KuitansiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Kuitansi::create([
+            'pengaju' => 'Kentang Mustofa',
+            'nominal' => 500000,
+            'terbilang' => 'LIMA RATUS RIBU RUPIAH',
+            'keperluan' => 'Kotak Infaq',
+            'jenis_kuitansi' => 'pemasukan',
+            'pembayaran' => 'tunai',
+        ]);
+        Kuitansi::create([
+            'pengaju' => 'Farhan Kebab',
+            'nominal' => 120000,
+            'terbilang' => 'SERATUS DUA PULUH RIBU RUPIAH',
+            'keperluan' => 'Uang Makan Basecamp',
+            'jenis_kuitansi' => 'penerimaan_barang',
+            'pembayaran' => 'transfer',
+        ]);
+        Kuitansi::create([
+            'pengaju' => 'Sigit Rendang',
+            'nominal' => 80000,
+            'terbilang' => 'DELAPAN PULUH RIBU RUPIAH',
+            'keperluan' => 'Pentasyarufan',
+            'jenis_kuitansi' => 'pengeluaran',
+            'pembayaran' => 'lainnya',
+        ]);
+    }
+}
