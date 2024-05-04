623579571425e8907d0acb512dfd7c5a313aa798
yudistiraramadan <yudistiraramadankalimasada@gmail.com>
2024-05-04 11:45:42 +0700

    styling 1

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -21,9 +21,9 @@
                     <div class="row">
                         <div class="col-12">
                             <div class="card">
-                                <div class="card-header">
-                                    {{-- <h5 class="card-title mb-0">Empty card</h5> --}}
-                                </div>
+                                {{-- <div class="card-header">
+                                    <h5 class="card-title mb-0">Empty card</h5>
+                                </div> --}}
                                 <div class="card-body">
                                     {{ $slot }}
                                 </div>
--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,1 +1,1 @@
-<h1 class="h3 mb-3">{{ $slot }}</h1>
+<h1 style="font-family:'Nunito'; font-weight:700; color:#222E3C; font-size:24px;" class="h3 mb-3">{{ $slot }}</h1>
