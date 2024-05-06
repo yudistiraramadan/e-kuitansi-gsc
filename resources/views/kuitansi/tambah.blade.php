3e251e29917764ffd50176ff8322056f983cf0dd
yudistiraramadan <yudistiraramadankalimasada@gmail.com>
2024-05-06 12:53:35 +0700

    konversi nominal to terbilang

--- resources/views/kuitansi/tambah.blade.php
+++ resources/views/kuitansi/tambah.blade.php
@@ -1,7 +1,56 @@
 <x-main>
     <x-slot:title>{{ $title }}</x-slot:title>
     <script>
-        function formatAngka(input) {
+        var huruf = [
+            '',
+            'Satu',
+            'Dua',
+            'Tiga',
+            'Empat',
+            'Lima',
+            'Enam',
+            'Tujuh',
+            'Delapan',
+            'Sembilan',
+            'Sepuluh',
+            'Sebelas',
+        ];
+
+        // Fungsi untuk mengonversi angka ke terbilang (bahasa Indonesia)
+        function convertToWords(nilai) {
+            var penyimpanan = '';
+            if (nilai < 12) {
+                penyimpanan = ' ' + huruf[nilai];
+            } else if (nilai < 20) {
+                penyimpanan = convertToWords(Math.floor(nilai - 10)) + ' Belas';
+            } else if (nilai < 100) {
+                var bagi = Math.floor(nilai / 10);
+                penyimpanan = convertToWords(bagi) + ' Puluh' + convertToWords(nilai % 10);
+            } else if (nilai < 200) {
+                penyimpanan = ' Seratus' + convertToWords(nilai - 100);
+            } else if (nilai < 1000) {
+                var bagi = Math.floor(nilai / 100);
+                penyimpanan = convertToWords(bagi) + ' Ratus' + convertToWords(nilai % 100);
+            } else if (nilai < 2000) {
+                penyimpanan = ' Seribu' + convertToWords(nilai - 1000);
+            } else if (nilai < 1000000) {
+                var bagi = Math.floor(nilai / 1000);
+                penyimpanan = convertToWords(bagi) + ' Ribu' + convertToWords(nilai % 1000);
+            } else if (nilai < 1000000000) {
+                var bagi = Math.floor(nilai / 1000000);
+                penyimpanan = convertToWords(bagi) + ' Juta' + convertToWords(nilai % 1000000);
+            } else if (nilai < 1000000000000) {
+                var bagi = Math.floor(nilai / 1000000000);
+                penyimpanan = convertToWords(bagi) + ' Miliar' + convertToWords(nilai % 1000000000);
+            } else if (nilai < 1000000000000000) {
+                var bagi = Math.floor(nilai / 1000000000000);
+                penyimpanan = convertToWords(bagi) + ' Triliun' + convertToWords(nilai % 1000000000000);
+            }
+            return penyimpanan.trim();
+        }
+
+        // Fungsi untuk mengonversi angka menjadi terbilang dan menempatkannya pada input 'terbilang'
+        function konversiTerbilang(input) {
             // Menghapus titik sebelum melakukan format
             var number = input.value.replace(/\./g, '');
 
@@ -12,7 +61,13 @@
             number = number.replace(/\D/g, '');
 
             // Format angka dengan titik sebagai pemisah ribuan
-            input.value = number.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+            var formatted = number.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+            input.value = formatted;
+
+            // Konversi angka ke terbilang
+            var angka = parseInt(number, 10);
+            var terbilang = convertToWords(angka);
+            document.getElementById("terbilang").value = terbilang.toUpperCase() + ' RUPIAH';
         }
     </script>
 
@@ -49,8 +104,8 @@
             <div class="col-lg-6">
                 <div class="mb-3">
                     <label for="nominal" class="form-label">Nominal</label>
-                    <input type="text" class="form-control" id="nominal" name="nominal" onkeyup="formatAngka(this)"
-                        value="{{ old('nominal') }}">
+                    <input type="text" class="form-control" id="nominal" name="nominal"
+                        onkeyup="konversiTerbilang(this)" value="{{ old('nominal') }}">
                     @error('nominal')
                         <div class="text text-danger">
                             {{ $message }}
@@ -61,7 +116,7 @@
             <div class="col-lg-6">
                 <div class="mb-3">
                     <label for="terbilang" class="form-label">Terbilang</label>
-                    <input type="text" class="form-control" id="terbilang" name="terbilang"
+                    <input type="text" class="form-control" id="terbilang" name="terbilang" readonly
                         value="{{ old('terbilang') }}">
                     @error('terbilang')
                         <div class="text text-danger">
