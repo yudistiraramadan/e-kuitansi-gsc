e1ade3cfab0905de396eb38bb545a28cddf2c3ad
yudistiraramadan <yudistiraramadankalimasada@gmail.com>
2024-05-08 20:46:39 +0700

    production

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,13 +4,15 @@
             <div class="col-6 text-start">
                 <p class="mb-0">
                     <a class="text-muted" href="https://www.instagram.com/yudistira8404/" target="_blank"><strong>TeamIT
-                            GSC</strong></a> &copy;
+                            GSC</strong></a> &copy; 2024
                 </p>
             </div>
             <div class="col-6 text-end">
                 <ul class="list-inline">
                     <li class="list-inline-item">
+
                         <a class="text-muted" href="https://wa.me/6285228409840" target="_blank">Help and Support</a>
+                        | Version 2.1
                     </li>
                 </ul>
             </div>
